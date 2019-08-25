cbaa19380611cd80d8975755684599be49f5ead7
lyrahgames <markuspawellek@gmail.com>
2019-08-25 15:42:14 +0200

    Add complete basic viewer

--- viewer.cpp
+++ viewer.cpp
@@ -1,9 +1,121 @@
 #include <viewer.hpp>
 
-viewer::viewer(QOpenGLWidget *parent) : QOpenGLWidget(parent) { show(); }
+#include <QApplication>
+#include <QKeyEvent>
+#include <QMouseEvent>
+#include <QWheelEvent>
 
-void viewer::initializeGL() {}
+#include <cmath>
+#include <random>
 
-void viewer::resizeGL(int width, int height) {}
+#include <glm/ext.hpp>
 
-void viewer::paintGL() {}
+using namespace std;
+
+viewer::viewer(QOpenGLWidget* parent) : QOpenGLWidget(parent) {
+  setMouseTracking(true);
+  show();
+
+  mt19937 rng{std::random_device{}()};
+  uniform_real_distribution<float> distribution{};
+  constexpr int count = 100;
+  data.reserve(3 * count);
+  for (int i = 0; i < 3 * count; ++i) data.push_back(distribution(rng));
+}
+
+void viewer::initializeGL() {
+  // glEnable(GL_MULTISAMPLE);
+  glClearColor(1, 1, 1, 1);
+}
+
+void viewer::resizeGL(int width, int height) {
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  glViewport(0, 0, width, height);
+  glLoadMatrixf(glm::value_ptr(glm::perspective(
+      glm::degrees(vertical_fov_), static_cast<float>(width) / height,
+      min_distance_, max_distance_)));
+  glMatrixMode(GL_MODELVIEW);
+  compute_look_at();
+}
+
+void viewer::paintGL() {
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+  glPointSize(5);
+  for (int i = 0; i < data.size(); i += 3) {
+    glBegin(GL_POINTS);
+    glColor3f(0, 0, 0);
+    glVertex3f(data[i], data[i + 1], data[i + 2]);
+    glEnd();
+  }
+}
+
+void viewer::mouseMoveEvent(QMouseEvent* event) {
+  const auto x_difference = event->x() - old_mouse_x_;
+  const auto y_difference = event->y() - old_mouse_y_;
+
+  switch (event->buttons()) {
+    case Qt::LeftButton:
+      rotate_on_screen(x_difference, y_difference);
+      break;
+
+    case Qt::RightButton:
+      move_origin_on_screen(x_difference, y_difference);
+      break;
+  }
+
+  old_mouse_x_ = event->x();
+  old_mouse_y_ = event->y();
+}
+
+void viewer::wheelEvent(QWheelEvent* event) {
+  constexpr float scale_factor = 0.003;
+  zoom(scale_factor * event->angleDelta().y());
+}
+
+void viewer::keyPressEvent(QKeyEvent* event) {
+  if (event->key() == Qt::Key_Escape) QApplication::quit();
+}
+
+void viewer::compute_look_at() {
+  const glm::vec3 world_up{0, 1, 0};
+  direction_ = {cos(eye_azimuth_) * cos(eye_altitude_), sin(eye_altitude_),
+                sin(eye_azimuth_) * cos(eye_altitude_)};
+  up_ = glm::normalize(world_up - glm::dot(direction_, world_up) * direction_);
+  right_ = glm::cross(up_, direction_);
+  const glm::vec3 position = origin_ + eye_distance_ * direction_;
+
+  makeCurrent();
+  glLoadIdentity();
+  glLoadMatrixf(glm::value_ptr(glm::lookAt(position, origin_, world_up)));
+  update();
+}
+
+float viewer::pixel_size() const noexcept {
+  return 2 * tan(0.5 * vertical_fov_) / height();
+}
+
+void viewer::move_origin_on_screen(int x_difference,
+                                   int y_difference) noexcept {
+  origin_ += pixel_size() * eye_distance_ *
+             (-static_cast<float>(x_difference) * right_ +
+              static_cast<float>(y_difference) * up_);
+  compute_look_at();
+}
+
+void viewer::rotate_on_screen(int x_difference, int y_difference) noexcept {
+  constexpr float max_altitude = 0.5 * pi_ - 1e-5;
+  constexpr float scale_factor = 0.01;
+
+  eye_azimuth_ += x_difference * scale_factor;
+  eye_altitude_ += y_difference * scale_factor;
+  eye_altitude_ = clamp(eye_altitude_, -max_altitude, max_altitude);
+  compute_look_at();
+}
+
+void viewer::zoom(float scale) noexcept {
+  eye_distance_ *= exp(-scale);
+  eye_distance_ = clamp(eye_distance_, min_distance_, max_distance_);
+  compute_look_at();
+}
--- viewer.hpp
+++ viewer.hpp
@@ -1,14 +1,15 @@
 #pragma once
 
 #include <QOpenGLWidget>
-#include <QPaintEvent>
-#include <QPainter>
+#include <glm/vec3.hpp>
+
+#include <vector>
 
 class viewer : public QOpenGLWidget {
   Q_OBJECT
 
  public:
-  explicit viewer(QOpenGLWidget *parent = nullptr);
+  explicit viewer(QOpenGLWidget* parent = nullptr);
   ~viewer() = default;
 
  protected:
@@ -16,5 +17,35 @@ class viewer : public QOpenGLWidget {
   void resizeGL(int width, int height) override;
   void paintGL() override;
 
+  void mouseMoveEvent(QMouseEvent* event) override;
+  void wheelEvent(QWheelEvent* event) override;
+  void keyPressEvent(QKeyEvent* event) override;
+
  private:
+  void compute_look_at();
+  float pixel_size() const noexcept;
+  void move_origin_on_screen(int x_difference, int y_difference) noexcept;
+  void rotate_on_screen(int x_difference, int y_difference) noexcept;
+  void zoom(float scale) noexcept;
+
+ private:
+  static constexpr float pi_ = 3.141592654;
+
+  // camera/frustum state
+  float vertical_fov_{0.25 * pi_};
+  float min_distance_ = 1e-3f;
+  float max_distance_ = 1000;
+  glm::vec3 origin_{};
+  float eye_distance_{10};
+  float eye_azimuth_{};
+  float eye_altitude_{};
+  glm::vec3 up_{};
+  glm::vec3 right_{};
+  glm::vec3 direction_{};
+
+  // cache
+  int old_mouse_x_{};
+  int old_mouse_y_{};
+
+  std::vector<float> data;
 };
